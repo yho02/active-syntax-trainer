730335ee24a5282b6db9529189d817e4bd618863
yho02 <yhotn2003@gmail.com>
2026-05-14 12:58:44 -0700

    grammar_engine set up

---
+++ core/grammar_engine.py
@@ -0,0 +1,4 @@
+def get_step(step):
+    return #retrieve step from file
+
+def get_level()
--- csv_to_json.py
+++ csv_to_json.py
@@ -1,4 +1,7 @@
 import pandas as pd
-import json as js
+import json 
 
-df = pd.read_csv("grammar_profile_cleaned")
+df = pd.read_csv("grammar_profile_ver2.csv")
+grammar_profile = df.to_dict('records')
+with open("data/grammar_profile.json", "w") as f:
+    json.dump(grammar_profile, f, indent=2)
